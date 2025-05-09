f051567bb683c1da8d90b21070ba89b1f7877044
siyaar <shubham.21b1531196@abes.ac.in>
2025-05-10 01:02:17 +0530

    bugs

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -49,31 +49,28 @@ export const registerUser = async (req, res) => {
 
 }
 
-
-//Login User:/api/user/login
+//LoginFunction:/api/user/login
 export const loginUser = async (req, res) => {
     const { email, password } = req.body;
+
     try {
-        // Check if user exists
-        if(!email||!password){
-            return res.json({success:false,message:"Please fill all the fields"})}
+        if (!email || !password) {
+            return res.json({ success: false, message: "Please fill all the fields" });
+        }
 
         const user = await User.findOne({ email });
         if (!user) {
-            return res.json({ success: false, message: "Invalid credentials" })}
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
 
-        // Check password
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.json({ success: false, message: "Invalid credentials" })}
-        
-        
-        // Send response  
-        
-        
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
+
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
             expiresIn: "7d",
-        }); 
+        });
 
         res.cookie("token", token, {
             httpOnly: true,
@@ -82,14 +79,16 @@ export const loginUser = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
-        return res.json({success:true,user:{email:user.email,name:user.name}})
+        return res.json({
+            success: true,
+            user: { email: user.email, name: user.name },
+        });
 
-
-         
+    } catch (error) {
+        console.log(error.message);
+        return res.json({ success: false, message: error.message });
     }
-    catch(error){ console.log(error.message);
-            res.json({success:false,message:error.message})}}
-
+};
 
 
     //check Auth:/api/user/is-auth
