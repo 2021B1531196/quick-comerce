ea4b084ced8f963bc5bd4a77803d2d2e90a2f7ea
siyaar <shubham.21b1531196@abes.ac.in>
2025-05-10 00:21:27 +0530

    fixing issues

--- server/middlewares/authUser.js
+++ server/middlewares/authUser.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 
 const authUser = (req, res, next) => {
-    const {token} = req.cookies.token;
+    const token = req.cookies.token;
     if (!token) {
         return res.json({ success: false, message: "Unauthorized" });
     }
