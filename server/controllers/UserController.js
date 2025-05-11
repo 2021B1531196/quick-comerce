6785c788589cfcaaa662e5bdf0e94ea22d19b62c
Shubham <121396486+2021B1531196@users.noreply.github.com>
2025-05-11 17:48:32 +0530

    Update UserController.js

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -32,7 +32,7 @@ export const registerUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
@@ -75,7 +75,7 @@ export const loginUser = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         });
 
@@ -111,7 +111,7 @@ export const logoutUser = async (req, res) => {
         res.clearCookie("token",{
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
         });
         return res.json({ success: true, message: "Logged out successfully" });
     } catch (error) {
